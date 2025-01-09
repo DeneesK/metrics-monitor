0a03360db9bd33ebbd7b272e831b8cc1b3877b9e
Denis Kalbasov <kds-ru@list.ru>
2025-01-09 11:01:39 +0100

    ping

--- internal/app/router/router.go
+++ internal/app/router/router.go
@@ -3,6 +3,7 @@ package router
 import (
 	"log/slog"
 
+	"github.com/DeneesK/metrics-monitor/internal/app/handlers"
 	"github.com/go-chi/chi/v5"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
@@ -10,5 +11,6 @@ import (
 func NewRouter(log *slog.Logger) *chi.Mux {
 	mux := chi.NewMux()
 	mux.Handle("/metrics", promhttp.Handler())
+	mux.Get("/ping", handlers.PingHandler)
 	return mux
 }
